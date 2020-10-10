4f7982326eeb7f4f8ef64df46456584b00efd2d6
Nimo Beeren <nimobeeren@gmail.com>
2020-10-10 12:23:11 +0200

    simplify stow command

--- setup/setup.sh
+++ setup/setup.sh
@@ -10,12 +10,8 @@ sudo chsh -s /usr/bin/zsh $(whoami) # sudo so it doesn't ask for password again
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Symlink dotfiles
-export STOW_DIR=".." # where the dotfiles come from
-export TARGET_DIR=$HOME # where the dotfiles go
-stow git --target=$TARGET_DIR
-stow mpd --target=$TARGET_DIR
-stow ssh --target=$TARGET_DIR
-rm $TARGET_DIR/.zshrc && stow zsh --target=$TARGET_DIR
+rm $HOME/.zshrc
+STOW_DIR=".." stow git mpd ssh zsh --target=$HOME/test
 
 # Install node
 zsh ./node.sh
