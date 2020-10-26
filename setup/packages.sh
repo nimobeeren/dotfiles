6b9840b97587a5f6cccd21343e7ad7fea2b05f70
Nimo Beeren <nimobeeren@gmail.com>
2020-10-26 18:58:12 +0100

    add stow to packages

--- setup/packages.sh
+++ setup/packages.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt install -y git zsh curl make gettext gnome-tweaks mpd
+sudo apt install -y git stow zsh curl make gettext gnome-tweaks mpd
