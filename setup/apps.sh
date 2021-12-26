57ef127f059bb0cb737f2185b2be8c7d25bbddaf
Nimo Beeren <nimobeeren@gmail.com>
2021-12-26 17:18:08 +0100

    add syncthing to apps

--- setup/apps.sh
+++ setup/apps.sh
@@ -9,4 +9,9 @@ sudo snap install emote
 
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt update && sudo apt install spotify-client
+
+sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+sudo apt update
+sudo apt install spotify-client syncthing
