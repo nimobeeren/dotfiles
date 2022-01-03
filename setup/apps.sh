5d6ae5695e9aa613f91318cf19c89416551c9436
Nimo Beeren <nimobeeren@gmail.com>
2022-01-03 20:25:51 +0100

    add github cli

--- setup/apps.sh
+++ setup/apps.sh
@@ -7,11 +7,17 @@ sudo snap connect glimpse-editor:removable-media # enable access to files outsid
 sudo snap install telegram-desktop
 sudo snap install emote
 
+# Spotify
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
+# Syncthing
 sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
 
+# GitHub CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
 sudo apt update
-sudo apt install spotify-client syncthing
+sudo apt install spotify-client syncthing gh
